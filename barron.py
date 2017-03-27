c5f36eabc72397bc67744228faee615730a45519
Czxck001 <Czxck001@outlook.com>
2017-03-27 14:52:10 +0800

    add script to grab a barron 800 wordlist

---
+++ barron.py
@@ -0,0 +1,36 @@
+''' Grab the Barron 800 high-frequency word list
+'''
+import re
+import requests
+
+barron_url = r'https://quizlet.com/6876275/'\
+    r'barrons-800-high-frequency-gre-word-list-flash-cards/'
+
+pattern = re.compile(
+    '<div class="SetPage-term">'
+    '<div class="SetPageTerm">'
+    '<div class="SetPageTerm-inner">'
+    '<div class="SetPageTerm-contentWrapper">'
+    '<div class="SetPageTerm-content">'
+    '<div class="SetPageTerm-word">'
+    '<div class="SetPageTerm-wordText">'
+    '<span class="TermText notranslate lang-en">'
+    '([A-Z]*)'
+    '</span></div></div>'
+    '<div class="SetPageTerm-definition">'
+    '<div class="SetPageTerm-definitionText">'
+    '<span class="TermText notranslate lang-en">'
+    '([A-Z,; ]*)'
+    '</span></div></div></div></div></div></div></div>'
+)
+
+
+def parse_wordlist(url):
+    html = requests.get(url).text
+    results = pattern.findall(html)
+    return [(word.lower(), defi.lower())
+            for word, defi in results]
+
+
+if __name__ == '__main__':
+    print(parse_wordlist(barron_url))
